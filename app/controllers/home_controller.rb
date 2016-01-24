73945114eaace5ab03a5480d9251d1e00aa70173
Gary Rosales <gary.rosales@walmart.com>
2016-01-24 13:27:53 -0500

    Added new pictures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #
-  before_action :authenticate_user!
+#  before_action :authenticate_user!
 
   def index
     @weddingInfo = WeddingInfo.all[0]
